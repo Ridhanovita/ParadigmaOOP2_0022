da6bc5dbf624c5cce7e134f191d9c7d430b661a1
Ridhanovita <novitaridha796@gmail.com>
2025-06-02 11:16:58 +0700

    deklarasi class budi turunan dari class pekerja dan class pelajar

--- ParadigmaOOP2/solveMultiwaris.cpp
+++ ParadigmaOOP2/solveMultiwaris.cpp
@@ -31,3 +31,14 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
